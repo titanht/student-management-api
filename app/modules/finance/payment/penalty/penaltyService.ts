8aa08da15845bf315c057a03342601a76eabbf39
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-12-22 22:05:38 +0300

    Penalty fix

--- app/modules/finance/payment/penalty/penaltyService.ts
+++ app/modules/finance/payment/penalty/penaltyService.ts
@@ -11,25 +11,27 @@ export default class PenaltyService {
     if (!months.includes(month)) {
       throw new Error('Invalid month value');
     }
+    let parsedEndTime: number[];
 
-    if (!endTime) {
-      endTime = moment(new Date()).format('YYYY-M-D');
+    if (endTime) {
+      parsedEndTime = endTime.split('-').map((i) => parseInt(i, 10));
+    } else {
+      const end = moment();
+      parsedEndTime = [end.year(), end.month(), end.date()];
     }
 
     const etYear = timeUtils.getEthiopianYear();
-    const etMonth = months.indexOf(month) + 1;
+    const etMonth = months.indexOf(month);
     const etDay = 1;
 
-    const startTime = timeUtils
-      .convertToGregorian(etYear, etMonth, etDay)
-      .join('-');
+    const startTime = timeUtils.convertToGregorian(etYear, etMonth, etDay);
 
     const rule = await PenaltyConfig.firstOrFail();
     const calculator = new PenaltyCalculator(rule);
-    const days =
-      moment(endTime, 'YYYY-M-D').diff(moment(startTime, 'YYYY-M-D')) /
-      (24 * 3600 * 1000);
+    const days = timeUtils.getDayDiff(parsedEndTime, startTime);
+
     let penalty = calculator.calculatePenalty(days);
+    console.log({ days, endTime, startTime, penalty });
     if (penalty > rule.max_fee) {
       penalty = rule.max_fee;
     }
