014244b9dfcdf51ca3096bf3ffede07fd62d35b1
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2022-10-05 17:27:46 +0300

    reporting api detailed

--- app/modules/finance/paymentLatest/fixed/fixedStudentPayment/lib/fixedStudentPaymentReportService.ts
+++ app/modules/finance/paymentLatest/fixed/fixedStudentPayment/lib/fixedStudentPaymentReportService.ts
@@ -1,22 +1,63 @@
+import FixedPayment from '../../fixedPayment';
 import FixedStudentPayment from '../fixedStudentPayment';
 
 const FixedStudentPaymentReport = {
+  parsePayments: (studentPayments: FixedStudentPayment[]) => {
+    const paymentMap: Record<
+      string,
+      {
+        fixed: FixedPayment;
+        amountTotal: number;
+        penaltyTotal: number;
+        count: number;
+      }
+    > = {};
+
+    studentPayments.forEach((payment) => {
+      if (paymentMap[payment.fixed_payment_id] === undefined) {
+        paymentMap[payment.fixed_payment_id] = {
+          fixed: payment.fixedPayment,
+          amountTotal: 0,
+          penaltyTotal: 0,
+          count: 0,
+        };
+      }
+
+      const curPayment = paymentMap[payment.fixed_payment_id];
+
+      paymentMap[payment.fixed_payment_id] = {
+        fixed: payment.fixedPayment,
+        amountTotal: curPayment.amountTotal + payment.amount,
+        penaltyTotal: curPayment.penaltyTotal + payment.penalty,
+        count: curPayment.count + 1,
+      };
+    });
+
+    return Object.values(paymentMap).sort((a, b) =>
+      a.fixed.description.localeCompare(b.fixed.description)
+    );
+  },
+
   report: async (startDate: string, endDate: string) => {
-    const payments = await FixedStudentPayment.query()
+    const studentPayments = await FixedStudentPayment.query()
+      .preload('fixedPayment')
       .where('created_at', '>=', `${startDate} 00:00:00`)
       .where('created_at', '<=', `${endDate} 23:59:59`);
 
-    const amountTotal = payments
+    const amountTotal = studentPayments
       .map((i) => i.amount)
       .reduce((a, b) => a + b, 0);
-    const penaltyTotal = payments
+    const penaltyTotal = studentPayments
       .map((i) => i.penalty)
       .reduce((a, b) => a + b, 0);
 
+    const payments = FixedStudentPaymentReport.parsePayments(studentPayments);
+
     return {
+      payments,
       amountTotal,
       penaltyTotal,
-      count: payments.length,
+      count: studentPayments.length,
     };
   },
 };
--- app/modules/finance/paymentLatest/recurrent/recurrentStudentPayment/lib/recurrentStudentPaymentReportService.ts
+++ app/modules/finance/paymentLatest/recurrent/recurrentStudentPayment/lib/recurrentStudentPaymentReportService.ts
@@ -1,22 +1,112 @@
+import RecurrentPayment from '../../recurrentPayment';
+import RecurrentPaymentChild from '../../recurrentPaymentChild/recurrentPaymentChild';
 import RecurrentStudentPayment from '../recurrentStudentPayment';
 
 const RecurrentStudentPaymentReportService = {
+  parseReports: (payments: RecurrentStudentPayment[]) => {
+    const paymentMap: Record<
+      string,
+      {
+        recurrent: RecurrentPayment;
+        recurrentChildren: Record<
+          string,
+          {
+            payment: RecurrentPaymentChild;
+            amountTotal: number;
+            penaltyTotal: number;
+            count: number;
+          }
+        >;
+        amountTotal: number;
+        penaltyTotal: number;
+        count: number;
+      }
+    > = {};
+
+    payments.forEach((payment) => {
+      if (paymentMap[payment.recurrent_payment_id] === undefined) {
+        paymentMap[payment.recurrent_payment_id] = {
+          recurrent: payment.recurrentPayment,
+          recurrentChildren: {},
+          amountTotal: 0,
+          penaltyTotal: 0,
+          count: 0,
+        };
+      }
+      if (
+        paymentMap[payment.recurrent_payment_id].recurrentChildren[
+          payment.recurrent_payment_child_id
+        ] === undefined
+      ) {
+        paymentMap[payment.recurrent_payment_id].recurrentChildren[
+          payment.recurrent_payment_child_id
+        ] = {
+          payment: payment.recurrentPaymentChild,
+          amountTotal: 0,
+          penaltyTotal: 0,
+          count: 0,
+        };
+      }
+
+      const curPayment = paymentMap[payment.recurrent_payment_id];
+      const curChildPayment =
+        paymentMap[payment.recurrent_payment_id].recurrentChildren[
+          payment.recurrent_payment_child_id
+        ];
+
+      paymentMap[payment.recurrent_payment_id] = {
+        recurrent: payment.recurrentPayment,
+        recurrentChildren: {
+          ...curPayment.recurrentChildren,
+          [payment.recurrent_payment_child_id]: {
+            payment: payment.recurrentPaymentChild,
+            amountTotal: curChildPayment.amountTotal + payment.amount,
+            penaltyTotal: curChildPayment.penaltyTotal + payment.penalty,
+            count: curChildPayment.count + 1,
+          },
+        },
+        amountTotal: curPayment.amountTotal + payment.amount,
+        penaltyTotal: curPayment.penaltyTotal + payment.penalty,
+        count: curPayment.count + 1,
+      };
+    });
+
+    const parsedPayments = Object.values(paymentMap)
+      .map((i) => ({
+        ...i,
+        recurrentChildren: Object.values(i.recurrentChildren).sort(
+          (a, b) => a.payment.order - b.payment.order
+        ),
+      }))
+      .sort((a, b) =>
+        a.recurrent.description.localeCompare(b.recurrent.description)
+      );
+
+    return parsedPayments;
+  },
+
   report: async (startDate: string, endDate: string) => {
-    const payments = await RecurrentStudentPayment.query()
+    const studentPayments = await RecurrentStudentPayment.query()
+      .preload('recurrentPayment')
+      .preload('recurrentPaymentChild')
       .where('created_at', '>=', `${startDate} 00:00:00`)
       .where('created_at', '<=', `${endDate} 23:59:59`);
 
-    const amountTotal = payments
+    const amountTotal = studentPayments
       .map((i) => i.amount)
       .reduce((a, b) => a + b, 0);
-    const penaltyTotal = payments
+    const penaltyTotal = studentPayments
       .map((i) => i.penalty)
       .reduce((a, b) => a + b, 0);
 
+    const payments =
+      RecurrentStudentPaymentReportService.parseReports(studentPayments);
+
     return {
+      payments,
       amountTotal,
       penaltyTotal,
-      count: payments.length,
+      count: studentPayments.length,
     };
   },
 };
